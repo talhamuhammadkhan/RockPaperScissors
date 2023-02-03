6856698134e4f8addb5138bbed00ade5f9e45a76
Talha Muhammad <106145159+talhamuhammadkhan@users.noreply.github.com>
2023-02-03 09:26:22 -0500

    delete unnecessary comments in Computer.java since method functionality is now obvious

--- src/Computer.java
+++ src/Computer.java
@@ -2,9 +2,6 @@ import java.util.Random;
 
 //This class will represent the computer who is playing with the user.
 public class Computer {
-	// This function helps the computer do the selection among ROCK,
-	// PAPER,SCISSORS
-	// we have used Random class from java.util.
 	public int chooseComputerWeapon() {
 		Random random = new Random();
 		int input = random.nextInt(3) + 1;
